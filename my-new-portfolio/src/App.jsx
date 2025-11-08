f11029a79620c50022a1f64e161afdd5e6ef096c
Sohan Weerasinghe <145052635+SohanWeerasinghe@users.noreply.github.com>
2025-11-08 16:55:31 +0530

    2nd commit - completed

--- my-new-portfolio/src/App.jsx
+++ my-new-portfolio/src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import Navbar from './sections/Navbar'
-import Hero from './sections/Hero';
-import About from './sections/About';
+import React from "react";
+import Navbar from "./sections/navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Experiences from "./sections/Experiences";
+import Testimonial from "./sections/Testimonial";
+import Contact from "./sections/Contact";
+import Footer from './sections/Footer';
 
 const App = () => {
   return (
-     <div className="container mx-auto max-w- relative">
+    <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
-      <About/>
-      <section className="min-h-screen "/>
-      <section className="min-h-screen "/>
+      <About />
+      <Projects />
+      <Experiences />
+      <Testimonial />
+      <Contact />
+      <Footer/>
     </div>
   );
 };
 
-export default App
+export default App;
